b07b7eae80ac867e817af5fed4c76eb2ca6d0220
nmwone <AirOne@unknown98fe9447285e.gateway.2wire.net>
2013-10-16 23:23:33 -0700

    Initial Commit

---
+++ Git Test/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Git Test
+//
+//  Created by nmwone on 10/16/13.
+//  Copyright (c) 2013 Oui Oui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Git Test
+//
+//  Created by nmwone on 10/16/13.
+//  Copyright (c) 2013 Oui Oui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
