6cf4a2ead83bdfce163d8dfbba22ee1c3fab89b1
nmwone <AirOne@unknown98fe9447285e.gateway.2wire.net>
2013-10-17 00:35:41 -0700

    added tests to class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by nmwone on 10/17/13.
+//  Copyright (c) 2013 Oui Oui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by nmwone on 10/17/13.
+//  Copyright (c) 2013 Oui Oui. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
